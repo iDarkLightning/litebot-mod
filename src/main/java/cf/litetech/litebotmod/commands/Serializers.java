863ee9549379ecb241debc8c32873773ecb668bb
iDarkLightning <nirjhor.nath@gmail.com>
2021-05-06 15:29:01 -0400

    added name and op_level to player serializer

--- src/main/java/cf/litetech/litebotmod/commands/Serializers.java
+++ src/main/java/cf/litetech/litebotmod/commands/Serializers.java
@@ -15,12 +15,16 @@ public class Serializers {
     public static String serializePlayer(ServerPlayerEntity player) {
         HashMap<String, Object> playerData = new HashMap<>();
 
+        playerData.put("name", player.getDisplayName().getString());
         playerData.put("uuid", player.getUuidAsString());
         playerData.put("dimension", player.getServer().getRegistryManager().get(Registry.DIMENSION_TYPE_KEY).
                 getId(player.getEntityWorld().getDimension()).toString());
         playerData.put("pos_x", player.getBlockPos().getX());
         playerData.put("pos_y", player.getBlockPos().getY());
         playerData.put("pos_z", player.getBlockPos().getZ());
+        playerData.put("op_level", LiteBotMod.getServer().getPlayerManager().isOperator(player.getGameProfile()) ?
+                LiteBotMod.getServer().getPlayerManager().getOpList().get(player.getGameProfile()).getPermissionLevel()
+                : 0);
 
         return new Gson().toJson(playerData);
     }
